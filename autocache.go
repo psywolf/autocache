cbe0c76de77d4e3c91cef8c53fddd85e3d7ec568
Zev <Zev.Goldstein@gmail.com>
2015-02-16 22:22:09 -0500

    fixed a bug when retrieving the same item twice in a row

--- autocache.go
+++ autocache.go
@@ -30,7 +30,6 @@ func New(maxSize int, lookupFunc LookupFuncType) *Cache {
 }
 
 func (c *Cache) Get(key string) (string, error) {
-
 	valNode, ok := c.hash[key]
 	chopTail := false
 	if !ok {
@@ -47,9 +46,15 @@ func (c *Cache) Get(key string) (string, error) {
 		}
 	}
 
-	//this lock may be overzealous
-	//erring on the side of caution for now
+	//if cache does not need to be modified
+	if valNode == c.head {
+		return valNode.val, nil
+	}
+
+	//this lock may not be in the correct or optimal place
+	//it seems to work well enough for now.
 	c.mutex.Lock()
+	//fmt.Printf("\ngetting word '%s'\n%s\n", key, c.debugFullCache())
 
 	//remove valnode from current place in tree
 	if valNode.prev != nil {
@@ -68,6 +73,7 @@ func (c *Cache) Get(key string) (string, error) {
 		}
 	}
 
+	//fmt.Printf("replacing head %#v\n", c.head)
 	//set valnode as head
 	valNode.prev = nil
 	if c.head != nil {
@@ -75,6 +81,7 @@ func (c *Cache) Get(key string) (string, error) {
 	}
 	valNode.next = c.head
 	c.head = valNode
+	//fmt.Printf("new head is %#v\n", c.head)
 
 	//on first get, set as tail too
 	if c.tail == nil {
@@ -82,11 +89,60 @@ func (c *Cache) Get(key string) (string, error) {
 	}
 
 	if chopTail {
+		//fmt.Printf("chopping tail %#v\n", c.tail)
 		delete(c.hash, c.tail.key)
 		c.tail = c.tail.prev
 		c.tail.next = nil
+		//fmt.Printf("new tail is %#v\n", c.tail)
 	}
 
 	c.mutex.Unlock()
 	return valNode.val, nil
+}
+
+func (n *node) String() string {
+	if n == nil {
+		return "NIL"
+	}
+
+	return "('" + n.key + "','" + n.val + "')"
+}
+
+func (n *node) GoString() string {
+	if n == nil {
+		return "NIL"
+	}
+
+	return n.prev.String() + " <- " + n.String() + " -> " + n.next.String()
+}
+
+func (c *Cache) debugFullCache() string {
+	headStr := ""
+	n := c.head
+	for {
+		if headStr != "" {
+			headStr += " -> "
+		}
+		headStr += n.String()
+
+		if n == nil {
+			break
+		}
+		n = n.next
+	}
+
+	tailStr := ""
+	n = c.tail
+	for {
+		if tailStr != "" {
+			tailStr += " -> "
+		}
+		tailStr += n.String()
+
+		if n == nil {
+			break
+		}
+		n = n.prev
+	}
+	return "FROM HEAD: " + headStr + "\nFROM TAIL: " + tailStr
 }
